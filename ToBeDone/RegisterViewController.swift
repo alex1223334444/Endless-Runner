a5f2941575d8b4c7be56ef69da8f17e7e8a099de
Taticu <82051653+Tatucu@users.noreply.github.com>
2022-10-26 17:22:40 +0300

    Create RegisterViewController.swift

---
+++ ToBeDone/RegisterViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// Register View Controller
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
