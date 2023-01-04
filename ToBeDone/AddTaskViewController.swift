39e61ccf7a06dcda0890972d3c3b7a6b4e31c450
Tatucu <andreid2312@gmail.com>
2023-01-04 22:53:14 +0100

    Big Fucking Commit

--- ToBeDone/AddTaskViewController.swift
+++ ToBeDone/AddTaskViewController.swift
@@ -7,16 +7,44 @@
 
 import UIKit
 
-class AddTaskViewController: UIViewController {
-
+class AddTaskViewController: UIViewController, TextFieldWithLabelDelegate {
+    func changeText(_ textContent: UITextField?) {
+        if let text=textContent?.text
+        {
+            print(text)
+        }
+    }
+    
+    @IBOutlet weak var PriorityPicker: UISegmentedControl!
+    @IBOutlet weak var TrackedBtn: UISwitch!
+    @IBOutlet weak var AlarmBtn: UISwitch!
+    @IBOutlet weak var CalendarPkr: UIDatePicker!
+    @IBAction func Tracked(_ sender: Any) {
+    }
+    
+    @IBAction func Priority(_ sender: Any) {
+    }
+    
+    @IBAction func Alarm(_ sender: Any) {
+    }
+    
+    @IBAction func Calendar(_ sender: Any) {
+    }
+    
     @IBOutlet weak var textfield: TextFieldWithLabel!
     @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var TaskTitle: TextFieldWithLabel!
+    
+    @IBOutlet weak var TaskDescription: TextFieldWithLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         button.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
-    }
+        
+        TaskTitle.configureTextField(with: "Title", tag:0, delegate: self)
+        TaskDescription.configureTextField(with: "Description", tag:1, delegate: self)    }
     
      @IBAction func pressSubmit(_ sender: Any) {
          print("submit")
@@ -27,5 +55,4 @@ class AddTaskViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    
 }
